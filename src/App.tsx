e3cf27700cf5d0eafc15c27954daa5518840d779
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2023-10-06 00:59:06 +0300

    added dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
+import '@reach/dialog/styles.css';
+import React from 'react';
+import { Dialog } from '@reach/dialog';
 import Logo from 'components/Logo';
 
 function App() {
+  const [openModal, setOpenModal] = React.useState('none');
+
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog aria-label="Register form" isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Register</h3>
+      </Dialog>
     </div>
   );
 }
